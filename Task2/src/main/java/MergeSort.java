18a5c5ee2537083a43baf93778438022656e33de
Alexxand <alexmich@yandex.ru>
2015-10-19 18:39:28 +0400

    In Task2: T[] changed in List<T>

--- Task2/src/main/java/MergeSort.java
+++ Task2/src/main/java/MergeSort.java
@@ -1,46 +1,41 @@
 import java.util.Comparator;
+import java.util.List;
+import java.util.ArrayList;
+
 
 public class MergeSort<T> {
-    private static <T> T[] merge(T[] arr1, T[] arr2, Comparator<T> comparator) {
-        int l1 = arr1.length;
-        int l2 = arr2.length;
-        T[] rez = (T[]) new Object[l1 + l2];
+    private static <T> List<T> merge(List<T> arr1, List<T> arr2, Comparator<T> comparator) {
+        int l1 = arr1.size();
+        int l2 = arr2.size();
+        List<T> rez = new ArrayList<T>(l1 + l2);
         int i = 0;
         int j = 0;
         while (i < l1 && j < l2) {
-            if (comparator.compare(arr1[i], arr2[j]) < 0) {
-                rez[i + j] = arr1[i];
-                ++i;
+            if (comparator.compare(arr1.get(i), arr2.get(j)) < 0) {
+                rez.add(arr1.get(i++));
             } else {
-                rez[i + j] = arr2[j];
-                ++j;
+                rez.add(arr2.get(j++));
             }
         }
         if (i == l1) {
             for (; j < l2; ++j)
-                rez[i + j] = arr2[j];
+                rez.add(arr2.get(j));
         } else {
             for (; i < l1; ++i)
-                rez[i + j] = arr1[i];
+                rez.add(arr1.get(i));
         }
         return rez;
     }
 
-    private static <T> T[] newarr(T[] arr, int first, int length) {
-        T[] rez = (T[])new Object[length];
-        for (int i = 0; i < length; ++i)
-            rez[i] = arr[first + i];
-        return rez;
-    }
 
-    public T[] mergesort(T[] arr, Comparator<T> comparator) {
-        int l = arr.length;
-        if (l != 1) {
-            T[] arr1 = newarr(arr, 0, l / 2);
-            T[] arr2 = newarr(arr, l / 2, (l + 1) / 2);
+    public List<T> mergesort(List<T> arr, Comparator<T> comparator) {
+        int l = arr.size();
+        if (l > 1) {
+            List<T> arr1 = arr.subList(0, l / 2);
+            List<T> arr2 = arr.subList(l / 2, (2 * l + 1) / 2);
             arr1 = mergesort(arr1, comparator);
             arr2 = mergesort(arr2, comparator);
-            return merge (arr1, arr2, comparator);
+            return merge(arr1, arr2, comparator);
         } else {
             return arr;
         }
--- Task2/src/test/java/SortTest.java
+++ Task2/src/test/java/SortTest.java
@@ -1,5 +1,3 @@
-
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,16 +6,18 @@ import org.junit.runners.Parameterized;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Vector;
+import java.util.ArrayList;
 
 @RunWith(Parameterized.class)
 public class SortTest {
-    private static final MergeSort<Double> SORT = new MergeSort();
+    private static final MergeSort<Double> SORT = new MergeSort<Double>();
     private static final Object[][] TEST_DATA = {
-            {SORT, new Double[]{1.0, 2.0, 3.0}},
-            {SORT, new Double[]{3.0, 2.0, 1.0}},
-            {SORT, new Double[]{1., 1., 1.}},
-            {SORT, new Double[]{-131., 2524., 23., -352.5}},
+            {SORT, Arrays.asList(new Double[]{1.0, 2.0, 3.0})},
+            {SORT, Arrays.asList(new Double[]{3.0, 2.0, 1.0})},
+            {SORT, Arrays.asList(new Double[]{3.0, 2.0, 7.0, 5.0, 5.0,3.12})},
+            {SORT, Arrays.asList(new Double[]{})}
     };
     private static final Comparator<Double> DOUBLE_COMPARATOR = new Comparator<Double>() {
         public int compare(final Double o1, final Double o2) {
@@ -26,36 +26,48 @@ public class SortTest {
     };
 
     @Parameterized.Parameters
-    public static Collection<Object[]> testData() {
+    public static Collection<Object[]> testData()
+    {
+
         return Arrays.asList(TEST_DATA);
     }
 
     private MergeSort<Double> sort;
-    private Double[] input;
+    private List<Double> input;
 
-    public SortTest(MergeSort<Double> sort, Double[] input) {
+    public SortTest(MergeSort<Double> sort, List<Double> input) {
         this.sort = sort;
         this.input = input;
     }
 
     @Test
     public void test() {
-        Double[] result = sort.mergesort(input, DOUBLE_COMPARATOR);
+        List<Double> result = sort.mergesort(input, DOUBLE_COMPARATOR);
         Assert.assertTrue("Result array should be sorted in ascending order", testAscendingOrder(result));
-        Assert.assertEquals("Result array length should be equal to original", input.length, result.length);
-        // Assert.assertTrue("Resalt array should contain all elemets of original",hasEachElementOf(,input, result));
+        Assert.assertEquals("Result array length should be equal to original", input.size(), result.size());
+        Assert.assertTrue("Result array should contain all elements of original",hasEachElementOf(input, result));
     }
 
-    private static boolean testAscendingOrder(Double[] array) {
-        for (int i = 0; i < array.length - 1; i++) {
-            if (array[i] > array[i + 1])
+    private static boolean testAscendingOrder(List<Double> array) {
+        for (int i = 0; i < array.size() - 1; i++) {
+            if (array.get(i) > array.get(i + 1))
                 return false;
         }
         return true;
     }
 
-        /*private static boolean hasEachElementOf(Double[] input, Double[] result) {
-            Vector<Double> temp = Vector(result);
-            return temp.containsAll(input);
-        }*/
+        private static boolean hasEachElementOf(List<Double> input, List<Double> result) {
+            List<Double> input_temp = new ArrayList<Double>(input);
+            List<Double> result_temp = new ArrayList<Double>(result);
+            while (!input_temp.isEmpty()) {
+                Double element = input_temp.get(0);
+                while (result_temp.remove(element)) {
+                  if (!(input_temp.remove(element)))
+                      return false;
+                }
+                if (input_temp.contains(element))
+                  return false;
+            }
+            return true;
+        }
 }
